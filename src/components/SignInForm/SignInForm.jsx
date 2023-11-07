bb784927e429d2760f511adfb0176f91059b73b3
김민승 <albert050509@naver.com>
2023-11-08 04:05:10 +0900

    bug : fix SignInForm Bug

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -59,6 +59,15 @@ const SignInForm = () => {
   );
 };
 
+const StSignIn = styled.form`
+  width: 100%;
+  margin-top: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #f0f1f5;
+`;
+
 const SignInContent = styled.div`
   width: calc(100% - 64vw);
   padding: 6vw;
