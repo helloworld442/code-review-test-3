e588ac1e7bf96d1eba43fb208fcee2ee257a9751
김민승 <albert050509@naver.com>
2023-11-21 11:59:21 +0900

    fix : fix getReviews api address

--- src/apis/review.js
+++ src/apis/review.js
@@ -1,7 +1,7 @@
 import api from "./instance";
 
 const getReviews = async () => {
-  const response = await api.get("/api/codeReview");
+  const response = await api.get("/api/codeReview?page=0&size=10");
   return response.data;
 };
 
